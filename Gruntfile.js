780410509ab29cfef7862db749f78fee8f0de6df
Sebastiaan Deckers <seb@ninja.sg>
2013-09-16 18:24:06 +0800

    Stylus and Nib upgraded and refactored as external dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,12 +65,27 @@ module.exports = function (grunt) {
 		},
 
 		stylus: {
-			options: {
-				compress: true
+			source: {
+				options: {
+					compress: false,
+					import: [
+						'nib'
+					]
+				},
+				files: {
+					'<%= source %>/styles/app.css': '<%= source %>/styles/app.styl'
+				}
 			},
-			file: {
-				src: '<%= staging %>/styles/app.styl',
-				dest: '<%= staging %>/styles/app.css'
+			staging: {
+				options: {
+					compress: true,
+					import: [
+						'nib'
+					]
+				},
+				files: {
+					'<%= staging %>/styles/app.css': '<%= staging %>/styles/app.styl'
+				}
 			}
 		},
 
@@ -337,7 +352,7 @@ module.exports = function (grunt) {
 		'imagemin',
 		'rev:assets',
 		'usemin:templates',
-		'stylus',
+		'stylus:staging',
 		'cssmin',
 		'usemin:css',
 		'rev:css',
